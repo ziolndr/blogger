0caaec354816852e7e6810dc323ca6cb6fe8d00d
ziolndr <ziolndr@gmail.com>
2013-07-21 13:07:54 -0700

    form completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,10 +12,37 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new
-		@article.title = params[:article][:title]
-		@article.body = params[:article][:body]
+		@article = Article.new(article_params)
 		@article.save
+
+		flash.notice = "Your article '#{@article.title}' has been created."
+
 		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		flash.notice = "Your article '#{@article.title}' has been deleted."
+
+		redirect_to articles_path
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Your article '#{@article.title}' is updated."
+
+		redirect_to article_path(@article)
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :body)
 	end
 end
